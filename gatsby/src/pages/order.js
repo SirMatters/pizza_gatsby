b025a5a8774c9bfc2441beacda98bbe481dec6ea
SirMatters <perov.krll@gmail.com>
2020-12-05 17:04:28 +0300

    Add Nav element and navigation links

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function Orders() {
   return (
     <div>
+      <Nav />
       <div>Orders</div>
     </div>
   );
--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function Pizzas() {
   return (
     <div>
+      <Nav />
       <div>Pizzas</div>
     </div>
   );
