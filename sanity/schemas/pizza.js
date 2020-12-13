164a860b61c76192ac410a226e36128a350ce937
SirMatters <perov.krll@gmail.com>
2020-12-13 20:50:05 +0300

    Sanity initial setup & pizza scheme

---
+++ sanity/schemas/pizza.js
@@ -0,0 +1,41 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  name: 'pizza',
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'Price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      validation: (Rule) => Rule.min(1000).max(50000),
+      // TODO: add a custom validation rule
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
